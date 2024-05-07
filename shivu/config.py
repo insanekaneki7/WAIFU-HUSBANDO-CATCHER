1dc4b56d05e841225850d9db3957a292a88b9277
Nikhil9792 <103943950+Nikhil9792@users.noreply.github.com>
2024-05-07 14:19:44 +0530

    Variables Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,17 +3,17 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6624008600"
-    sudo_users = "6624008600", "5208708688"
-    GROUP_ID = -1002093059683
-    TOKEN = "7535881054:AAFheRPYsLoXvG6RhH9fd9bkousj4JLdo9E"
-    mongo_url = "mongodb+srv://insanekaneki7:w86O8Q2f7pGEdWYR@cluster0.fxcecdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    PHOTO_URL = ["https://graph.org/file/de583389881d34e4b51e5.jpg", "https://graph.org/file/3eb21ef48c323d0b3f0c5.jpg"]
-    SUPPORT_CHAT = "insanesociety"
-    UPDATE_CHAT = "insaneupdate"
-    BOT_USERNAME = "insane_catcher_bot"
-    CHARA_CHANNEL_ID = "1002068358722"
-    api_id = 24341346
-    api_hash = "2e0526c7dbae85722313845219600c56"
+    sudo_users = "5391039247", "1486727193", "1102584877", "7052570049", "6391328971", "7100617938", "7150190248", "5165804914", "7027685257", "6975280846"
+    GROUP_ID = -1002102504910
+    TOKEN = ""
+    mongo_url = ""
+    PHOTO_URL = ["https://te.legra.ph/file/6f9313f01e1742cc561ed.jpg", "https://te.legra.ph/file/bf8bb65383c6aaf437daf.jpg", "https://te.legra.ph/file/5a6922a3e94ec102cff38.jpg", "https://te.legra.ph/file/539101180b5a9e53e3c71.jpg"]
+    SUPPORT_CHAT = "anime_chat_anicade"
+    UPDATE_CHAT = "anime_chat_anicade"
+    BOT_USERNAME = "guess_waifu_robot"
+    CHARA_CHANNEL_ID = "-1002091239384"
+    api_id = 6252332
+    api_hash = "2bf5e96a75ca073fd4f37ca9562971d3"
 
     
 class Production(Config):
